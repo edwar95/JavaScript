c76efc19257fb4d94a235dc6f8aee14a7ed81562
david <davidmoralesp1995@gmail.com>
2018-04-25 12:38:05 -0500

    clases

--- 02-Typescript/clases.js
+++ 02-Typescript/clases.js
@@ -1,1 +1,14 @@
 console.log("hola mundo");
+var hola = "hola";
+var adios = "adiós";
+var variables;
+var Usuario = /** @class */ (function () {
+    function Usuario(nombre, casado, edad) {
+        this.nombre = nombre;
+        this.casado = casado;
+        this.edad = edad;
+    }
+    return Usuario;
+}());
+var david = new Usuario("David", false, 22);
+console.log(david);
--- 02-Typescript/clases.ts
+++ 02-Typescript/clases.ts
@@ -1,1 +1,17 @@
-console.log("hola mundo")
+console.log("hola mundo");
+let hola="hola";
+const adios= "adiós";
+let variables:string;
+
+class Usuario{
+    public nombre:string;
+    private casado:boolean;
+    protected edad:number;
+    constructor(nombre:string, casado:boolean, edad:number) {
+        this.nombre = nombre;
+        this.casado = casado;
+        this.edad = edad;
+    }
+}
+let david :Usuario= new Usuario("David", false,22);
+console.log( david);

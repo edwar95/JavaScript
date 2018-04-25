83944bbdaec4f1894770d85cd88a20cf43777284
david <davidmoralesp1995@gmail.com>
2018-04-25 13:00:24 -0500

    get set y metodos en clases typescript

--- 02-Typescript/clases.js
+++ 02-Typescript/clases.js
@@ -12,3 +12,28 @@ var Usuario = /** @class */ (function () {
 }());
 var david = new Usuario("David", false, 22);
 console.log(david);
+var UsuarioDos = /** @class */ (function () {
+    function UsuarioDos(nombre, _casado, _edad) {
+        this.nombre = nombre;
+        this._casado = _casado;
+        this._edad = _edad;
+    }
+    Object.defineProperty(UsuarioDos.prototype, "casado", {
+        get: function () {
+            return this._casado;
+        },
+        set: function (casado) {
+            this._casado = casado;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    UsuarioDos.prototype.imrpimirUsuario = function (saludo) {
+        //template string
+        return saludo + ". Mi nombre es " + this.nombre + ", estoy casado " + this.casado + ", mi edad es " + this._edad;
+    };
+    return UsuarioDos;
+}());
+var david2 = new UsuarioDos("david", false, 22);
+console.log(david2);
+console.log(david2.imrpimirUsuario("hola"));
--- 02-Typescript/clases.ts
+++ 02-Typescript/clases.ts
@@ -15,3 +15,22 @@ class Usuario{
 }
 let david :Usuario= new Usuario("David", false,22);
 console.log( david);
+
+class UsuarioDos{
+    constructor(public nombre:string,  private _casado:boolean, protected _edad:number){
+    }
+    get casado(){
+        return this._casado;
+    }
+    set casado(casado:boolean){
+        this._casado=casado;
+    }
+    imrpimirUsuario(saludo:string):string{
+        //template string
+        return `${saludo}. Mi nombre es ${this.nombre}, estoy casado ${this.casado}, mi edad es ${this._edad}`;
+    }
+}
+
+let david2 :UsuarioDos=new UsuarioDos("david",false,22);
+console.log(david2)
+console.log(david2.imrpimirUsuario("hola"));
